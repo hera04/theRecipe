65d1e80590bbfec1ce9fde4053e227399a581078
Przemysław Hernik <przemek.hernik@hotmail.com>
2015-06-22 01:19:04 +0200

    Wstępny szablon 1

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).load(function () {
+
+    var top_margin = $(window).height() * 0.7;
+    //alert(top_margin);
+
+    $(document).scroll(function () {
+        $("#navigation").toggleClass("small", ($(this).scrollTop() > 0));
+        $("#siteContent").toggleClass("site-content-static", ($(this).scrollTop() > 0));
+    });
+
+    //if $(window).height() > 1024 {$("#siteContent").toggleClass("site-content");}
+
+});
+
+$('a[href*=#]').click(function () {
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top - 30
+    }, 1500);
+    return false;
+});
