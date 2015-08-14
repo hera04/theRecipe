51e888b8f6352a0841964853fcfa1ceb4daa1336
Przemysław Hernik <przemek.hernik@hotmail.com>
2015-08-14 17:28:38 +0200

    Praca z komentarzami - poprawić wyświetlanie komentarza rodzica

--- comments.php
+++ comments.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
     if( !empty($_SERVER['SCRIPT_FILENAME']) && 'comments.php'==basename($_SERVER['SCRIPT_FILENAME']) )
         die ('Nie możesz bezpośrednio uruchomić tego pliku');
@@ -19,7 +18,7 @@
 <div class="row">
     <div class="medium-12 column comments">
 
-        <h3 class="rc-subtitle">Komentarze do przepisu:</h3>
+        <h3 class="rc-subtitle">Komentarze do przepisu: <small><?php comments_number( '0 komentarzy', '1 komentarz', '% komentarzy' );?></small></h3>
 
         <?php if ( !comments_open() ) : ?>
             
@@ -29,8 +28,6 @@
 
                 <div class="row comment_list">
                     
-                    <a href="#"><h4 class="site-titles cm-title"><?php the_title(); ?> <small><?php comments_number( '0 komentarzy', '1 komentarz', '% komentarzy' );?></small></h4></a>
-
                     <!--<div class="comment">
                         <div style="font-size: 1.2em; color: #999;">#1</div>                            
                         <div class="parent inner">
@@ -73,10 +70,6 @@
                 
                 <div class="row respond"> 
 
-                    <header>
-                        <h5><?php comment_form_title('Dodaj komentarz','Odpowiedz na komentarz: %s');?></h5>
-                    </header>                           
-                    
                     <?php if ( get_option('comment_registration') && !is_user_logged_in() ) : ?>
                             
                         <p>Musisz się <a href="<?php wp_login_url(get_permalink()) ?>">zalogować</a></p>
@@ -87,15 +80,13 @@
 
                         <div id="addComent" class="reveal-modal" data-reveal aria-labelledby="modalTitle" aria-hidden="true" role="dialog" style="width:50%">
 
-                            <h5><?php comment_form_title('Dodaj komentarz','Odpowiedz na komentarz: %s');?></h5>
-
                             <?php
                                 $is_reply = checkUrlSingleParam('replytocom');
                             
                                 if ($is_reply):
                                     
                                     $parent_ID = get_comment($comment->comment_parent);
-                                    echo '<p class="panel">'.$parent_ID->comment_content.'</p>';
+                                    //echo '<p class="panel">'.$parent_ID->comment_content.'</p>';
                                     ?>
                                     <script type="text/javascript">
                                         $(document).foundation();
@@ -105,15 +96,73 @@
                                 endif;
                             ?>
 
-                            <form id="comment-form" method="post" action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php">
+                            <!--<form id="comment-form" method="post" action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php">-->
 
-                                <textarea name="comment" rows="5"></textarea>
-                                <?php comment_id_fields(); ?>
-                                <div class="left"><?php cancel_comment_reply_link('Anuluj odpowiedź.'); ?></div>
-                                <button type="submit" class="button tiny right">Dodaj Komentarz</button>
-                                <?php do_action('comment_form',$post->ID); ?>
+                                <?php
+                                
+                                    $commenter = wp_get_current_commenter();
+                                    $req = get_option( 'require_name_email' );
+                                    $aria_req = ( $req ? " aria-required='true'" : '' );
+                                
+                                    $fields =  array(
+                                        'author' =>
+                                          '<p class="comment-form-author"><label for="author">' . __( 'Name', 'domainreference' ) . '</label> ' .
+                                          ( $req ? '<span class="required">*</span>' : '' ) .
+                                          '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+                                          '" size="30"' . $aria_req . ' /></p>',
 
-                            </form>
+                                        'email' =>
+                                          '<p class="comment-form-email"><label for="email">' . __( 'Email', 'domainreference' ) . '</label> ' .
+                                          ( $req ? '<span class="required">*</span>' : '' ) .
+                                          '<input id="email" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+                                          '" size="30"' . $aria_req . ' /></p>',
+
+                                        'url' =>
+                                          '<p class="comment-form-url"><label for="url">' . __( 'Website', 'domainreference' ) . '</label>' .
+                                          '<input id="url" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+                                          '" size="30" /></p>',
+                                    );
+                                    
+                                    $args = array(
+                                      'id_form'           => 'commentform',
+                                      'id_submit'         => 'submit',
+                                      'class_submit'      => 'button tiny right',
+                                      'name_submit'       => 'submit',
+                                      'title_reply'       => 'Odpowiedz na post.',
+                                      'title_reply_to'    => 'Odpowiedz na komentarz %s: <blockquote class="cm-to-reply">'.$comment->comment_content.'</blockquote>',
+                                      'cancel_reply_link' => 'Anuluj odpowiedź',
+                                      'label_submit'      => 'Wyślij komentarz',
+                                      'format'            => 'xhtml',
+
+                                      'comment_field' =>  '<textarea name="comment" rows="5" placeholder="Wpisz treść komentarza"></textarea>',
+
+                                      'must_log_in' => '<p class="must-log-in">' .
+                                        sprintf(
+                                          __( 'You must be <a href="%s">logged in</a> to post a comment.' ),
+                                          wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
+                                        ) . '</p>',
+
+                                      'logged_in_as' => '<p class="logged-in-as">' .
+                                        sprintf(
+                                        __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>' ),
+                                          admin_url( 'profile.php' ),
+                                          $user_identity,
+                                          wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
+                                        ) . '</p>',
+
+                                      'comment_notes_before' => '<p class="comment-notes">' .
+                                        __( 'Your email address will not be published.' ) . ( $req ? $required_text : '' ) .
+                                        '</p>',
+
+                                      'comment_notes_after' => '',
+
+                                      'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+                                    );
+                                ?>
+
+                                <?php comment_form( $args, $post->ID ); ?>
+
+                            <!--</form>-->
                 
                         </div>
                             
